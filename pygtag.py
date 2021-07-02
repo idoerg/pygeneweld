41924f628ed48c7f60abc82626651269ae32d539
Iddo Friedberg <idoerg@gmail.com>
2021-07-02 17:42:15 -0500

    updted pygtag

--- pygtag.py
+++ pygtag.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 
+import sys
 import re
 from Bio import SeqIO
+from Bio import Entrez
 from Bio.Seq import Seq
 
 
@@ -21,20 +24,22 @@ def find_pams(seq_rec):
         pams.append((p1, p2, inseq[p1:p2]))
     return pams
 
-def find_upstream_arm_sense(seq_rec, pam, armlen = 48):
 
+def find_upstream_arm_sense(genomic_dna, guide_rna, guide_rna_span, armlen = 48):
+
+    p1, p2 = guide_rna_span
     # Page 20, Figure 20
-    p1, p2, pamstr = pam
-    cas9_site = p1 - 3
-    assert cas9_site - armlen > 0, f'{p2} is the pam position too close to 5` end'
+    cas9_site =  p2 - 3
     arm_p1 = cas9_site - armlen
-    upstream_arm = seq_rec[arm_p1:cas9_site]
-
+    arm_p2 = cas9_site
+    upstream_arm = genomic_dna.seq[arm_p1:arm_p2]
+    print(f"upstream arm: {upstream_arm}")
     # Found upstream arm, now find spacer
     # Spacer is a 3mer whose base is not found in -51 to -49
     # Page 16, item #2
-    spacer_base = find_spacer_base(seq_rec[arm_p1-3:arm_p1])
-    assert spacer_base, f'No spacer_base found in {seq_rec[arm_p1-3:arm_p1]}'
+    spacer_base = find_spacer_base(genomic_dna.seq[arm_p1-3:arm_p1])
+    print(f"bla bla: {genomic_dna.seq[arm_p1-3:p2]}")
+    assert spacer_base, f'No spacer_base found in {genomic_dna.seq[arm_p1-3:arm_p1]}'
     spacer_base *= 3
     upstream_arm = spacer_base + upstream_arm
 
@@ -46,24 +51,60 @@ def find_upstream_arm_sense(seq_rec, pam, armlen = 48):
     return upstream_arm
 
 def find_spacer_base(inseq):
+    print(f"inseq {str(inseq).upper()}")
     spacer_base = None
     for i in ['A','T','G','C']:
-        if not (i in upper(str(inseq)):
+        if not (i in str(inseq).upper()):
             spacer_base = i
             break
     return spacer_base 
 
-guide_rna=Seq("GCCACAGCATGTTTGGGCAT")
+guide_rna = Seq("GCCACAGCATGTTTGGGCAT")
+                  
+guide_rna_2 = Seq("CCTGTTTGATCACCCGTTAA").reverse_complement()
 
-def read_sequence(gdna_file, guide_rna):
+def get_genomic_dna(sequence_id, source, entrez_email=None, 
+        entrez_db=None, file_format=None):
+    # Source can be: local file, EnsEMBL ID, NCBI ID
+    if source.upper() == "ENSEMBL":
+        ensRest = EnsemblRest()
+        genomic_dna_ensembl = ensRest.getSequenceById(id=sequence_id)
+        genomic_dna = SeqRecord(id=genomic_dna_ensembl['id'],
+                      seq=genomic_dna_ensembl['seq'])
+    elif source.upper() == "NCBI" or source.upper() == "ENTREZ":
+        assert entrez_email and entrez_db, "Your email and entrez db required"
+        genomic_dna = next(Entrez.efetch(id=sequence_id, db="nucleotide", 
+                rettype="fasta", retmode="text"))
+    elif source.upper() == "FILE":
+        genomic_dna = next(SeqIO.parse(gdna_file, "fasta"))
+
+    else:
+        raise ValueError(f"Bad sequence source {source}")
+    return genomic_dna
+
+
+def find_guide_sequence(gdna_file, guide_rna=guide_rna, strand = 1):
     genomic_dna = next(SeqIO.parse(gdna_file, "fasta"))
     guide_rna = guide_rna.upper()
-    genomic_dna_seq = genomic_dna.seq.upper()
+    genomic_dna.seq = genomic_dna.seq.upper()
+    print(type(guide_rna), type(genomic_dna))
     assert len(guide_rna) == 20, f'guide RNA must be 20bp' 
-    guide_rna_location = re.search(str(guide_rna), str(genomic_dna.seq))
-    assert guide_rna_location, f'Guide RNA not in genomic DNA'
+    # Look for the guide RNA on the sense strand
+    if strand == 1:
+        guide_rna_location = re.search(str(guide_rna), str(genomic_dna.seq))
+    else:
+        guide_rna_location = re.search(str(guide_rna.reverse_complement()),
+            str(genomic_dna))
+    if not guide_rna_location:
+        raise ValueError(f'Guide RNA {guide_rna} not in genomic DNA')
     guide_rna_span = guide_rna_location.span()
 
-    return guide_rna, genomic_dna_seq, guide_rna_span
+    print(guide_rna, guide_rna_span)
+    return (guide_rna, genomic_dna, guide_rna_span)
+    
+if __name__ == '__main__':
+    guide_rna, genomic_dna, guide_rna_span = find_guide_sequence(sys.argv[1],strand=1)
+    upstream_arm = find_upstream_arm_sense(genomic_dna, guide_rna,guide_rna_span)
+    print(upstream_arm)
     
 
